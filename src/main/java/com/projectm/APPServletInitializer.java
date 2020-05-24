9fc57dd8ecabd2587b37508f8208ce7f370b2538
xuan99813 <463247428@qq.com>
2020-05-25 01:28:20 +0800

    1

--- src/main/java/com/projectm/APPServletInitializer.java
+++ src/main/java/com/projectm/APPServletInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /**
  * web容器中进行部署
  * 
- * @author ruoyi
+ * @author gencya
  */
 public class APPServletInitializer extends SpringBootServletInitializer
 {
